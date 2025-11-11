78920871a9807ed380f8b229c92a5679cdd8d1d0
Ayush Bhandarkar <ayushbhandarkar7@gmail.com>
2025-11-11 17:38:22 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from '../component/Title';
-import contact from "../assets/contact2.jpg";
+import contact from "../assets/contact3.jpg";
 import NewLetterBox from '../component/NewLetterBox';
 import { motion } from 'framer-motion';
 
